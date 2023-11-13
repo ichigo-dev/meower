7f7d58ccebd5ebdb0eb709883afc4c60741c4bf1
ichigo <dev.honda.ichigo@gmail.com>
2023-11-13 15:24:48 +0900

    feat: Update authentication proxy server

--- auth_proxy/src/main.rs
+++ auth_proxy/src/main.rs
@@ -3,25 +3,53 @@
 //------------------------------------------------------------------------------
 
 use std::net::SocketAddr;
+use std::env;
 
-use axum::routing::get;
-use axum::Router;
+use axum::response::Response;
+use axum::http::Request;
+use axum::body::Body;
+use tower::make::Shared;
 
+//------------------------------------------------------------------------------
+/// Main entry point.
+//------------------------------------------------------------------------------
 #[tokio::main]
 async fn main()
 {
-    let app = Router::new()
-        .route("/", get(handler));
+    let service = tower::service_fn(move |req: Request<_>|
+    {
+        async move { proxy(req).await }
+    });
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 9001));
+    // Run the server.
+    let port = env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .unwrap_or(8080);
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(Shared::new(service))
         .await
         .unwrap();
 }
 
-
-async fn handler() -> &'static str
+//------------------------------------------------------------------------------
+/// Proxies the request to the frontend.
+//------------------------------------------------------------------------------
+async fn proxy( req: Request<Body> ) -> Result<Response<Body>, hyper::Error>
 {
-    "Hello, World!"
+    let path = match req.uri().path_and_query()
+    {
+        Some(path) => path,
+        None => return Ok(Response::new(Body::empty())),
+    };
+    let to_req = Request::builder()
+        .uri("http://frontend:9000".to_string() + path.as_str())
+        .method(req.method())
+        .version(req.version())
+        .body(req.into_body())
+        .unwrap();
+
+    let client = hyper::Client::new();
+    client.request(to_req).await
 }
