818a7205184f0648ab1d15308f2a41bd2202e29d
ichigo-dev <dev.honda.ichigo@gmail.com>
2023-12-27 14:18:07 +0900

    feat: Multilingual support for SPA

--- dev/system/service/spa/src/main.rs
+++ dev/system/service/spa/src/main.rs
@@ -2,7 +2,13 @@
 //! SPA frontend for the Meower.
 //------------------------------------------------------------------------------
 
+use rust_i18n::t;
 use sycamore::prelude::*;
+
+use log::debug;
+
+// Loads the locales.
+rust_i18n::i18n!("locales");
 
 
 //------------------------------------------------------------------------------
@@ -11,10 +17,16 @@ use sycamore::prelude::*;
 #[component]
 pub fn Root<G: Html>( cx: Scope ) -> View<G>
 {
+    // Sets the locale.
+    let window = web_sys::window().unwrap();
+    let navigator = window.navigator();
+    let language = navigator.language().unwrap();
+    rust_i18n::set_locale(language.as_str());
+
     view!
     {
         cx,
-        "Hello, Meower"
+        (t!("hello"))
     }
 }
 
