c6e5395687994403b232ca2f9a4f6fed006e5064
ichigo <dev.honda.ichigo@gmail.com>
2024-01-02 20:17:30 +0900

    chore: Update UI catalog

--- dev/proto/ui_catalog/assets/js/index.js
+++ dev/proto/ui_catalog/assets/js/index.js
@@ -24,7 +24,7 @@ const theme_colors =
 	'primary', 'secondary', 'error', 'warning', 'info', 'success',
 ];
 
-const theme_color_types = ['', 'light', 'dark'];
+const theme_color_types = ['light', '', 'dark'];
 
 
 //------------------------------------------------------------------------------
