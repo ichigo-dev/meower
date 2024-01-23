26754294c6e1fbe08a9a91486721f0f7cc7c0b94
dev.honda.ichigo <dev.honda.ichigo@gmail.com>
2024-01-24 08:25:47 +0900

    chore: Remove dev proto

--- dev/system/app/spa/src/config.rs
+++ dev/system/app/spa/src/config.rs
@@ -2,6 +2,10 @@
 //! Configuration.
 //------------------------------------------------------------------------------
 
+use std::collections::HashMap;
+
+use wasm_bindgen::JsCast;
+
 
 //------------------------------------------------------------------------------
 /// Config.
@@ -10,6 +14,9 @@
 pub(crate) struct Config
 {
     pub(crate) api_url: String,
+    pub(crate) client_id: String,
+    pub(crate) user_token: String,
+    pub(crate) access_token: String,
 }
 
 impl Config
@@ -19,11 +26,30 @@ impl Config
     //--------------------------------------------------------------------------
     pub(crate) fn init() -> Self
     {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let html_document = document
+            .dyn_into::<web_sys::HtmlDocument>()
+            .unwrap();
+        let cookie = html_document.cookie().unwrap();
+        let cookies = cookie
+            .split("; ")
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|cookie| cookie.split("=").collect::<Vec<_>>())
+            .map(|cookie| (cookie[0].to_string(), cookie[1].to_string()))
+            .collect::<HashMap<_, _>>();
+
         let api_url = std::env!("API_URL").to_string();
+        let client_id = cookies.get("client_id").unwrap_or_default();
+        let user_token = cookies.get("user_token").unwrap_or_default();
+        let access_token = cookies.get("access_token").unwrap_or_default();
 
         Self
         {
             api_url,
+            client_id,
+            user_token,
+            access_token,
         }
     }
 }
