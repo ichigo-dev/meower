3ad18509ec2125d1f223d4260e3ec8cca16631e5
ichigo-dev <dev.honda.ichigo@gmail.com>
2024-02-02 14:57:58 +0900

    feat: Feat table component

--- dev/system/app/spa/src/layouts/application/main_panel.rs
+++ dev/system/app/spa/src/layouts/application/main_panel.rs
@@ -14,7 +14,7 @@ pub fn MainPanel<G: Html>( children: Children<G> ) -> View<G>
     let children = children.call();
     view!
     {
-        div(class="ui_box surface radius padding_lg flex flex_column flex_align_start flex_gap_md")
+        div(class="ui_box surface radius padding_lg flex flex_column flex_align_start flex_gap_md overflow_scroll_x")
         {
             (children)
         }
--- dev/system/app/spa/src/pages/dev/ui_catalog/examples/table.rs
+++ dev/system/app/spa/src/pages/dev/ui_catalog/examples/table.rs
@@ -15,6 +15,7 @@ use sycamore::prelude::*;
 #[component(inline_props)]
 pub fn TableExamples<G: Html>( colors: ReadSignal<Vec<String>> ) -> View<G>
 {
+    let iter = create_signal((1..=5).collect::<Vec<_>>());
     view!
     {
         h3(class="ui_heading h3")
@@ -25,11 +26,414 @@ pub fn TableExamples<G: Html>( colors: ReadSignal<Vec<String>> ) -> View<G>
         {
             div(class="flex flex_row flex_gap_md flex_align_center width_full")
             {
-                Table()
+                Table
                 {
-                    "hoge"
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                    TableFoot
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Footer " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+                Table
+                {
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (i)
+                                    }
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
                 }
             }
+            div(class="flex flex_row flex_gap_md flex_align_center width_full")
+            {
+                Table(size=*create_signal("small".to_string()))
+                {
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+                Table
+                {
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+                Table(size=*create_signal("large".to_string()))
+                {
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+            div(class="flex flex_row flex_gap_md flex_align_center width_full")
+            {
+                Table(variant=*create_signal("stripe".to_string()))
+                {
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+                Table(variant=*create_signal("tiled".to_string()))
+                {
+                    TableHead
+                    {
+                        TableRow
+                        {
+                            Indexed
+                            (
+                                iterable=*iter,
+                                view=|index| view!
+                                {
+                                    TableCell(is_head=*create_signal(true))
+                                    {
+                                        "Header " (index)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                    TableBody
+                    {
+                        Indexed
+                        (
+                            iterable=*iter,
+                            view=move |i| view!
+                            {
+                                TableRow
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |j| view!
+                                        {
+                                            TableCell
+                                            {
+                                                "Data " (i) "-" (j)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+            Indexed
+            (
+                iterable=colors,
+                view=move |color|
+                {
+                    let cloned_color = color.clone();
+                    view!
+                    {
+                        div(class="flex flex_row flex_gap_md flex_align_center width_full")
+                        {
+                            Table(color=*create_signal(cloned_color))
+                            {
+                                TableHead
+                                {
+                                    TableRow
+                                    {
+                                        Indexed
+                                        (
+                                            iterable=*iter,
+                                            view=|index| view!
+                                            {
+                                                TableCell(is_head=*create_signal(true))
+                                                {
+                                                    "Header " (index)
+                                                }
+                                            }
+                                        )
+                                    }
+                                }
+                                TableBody
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |i| view!
+                                        {
+                                            TableRow
+                                            {
+                                                Indexed
+                                                (
+                                                    iterable=*iter,
+                                                    view=move |j| view!
+                                                    {
+                                                        TableCell
+                                                        {
+                                                            "Data " (i) "-" (j)
+                                                        }
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                            Table(color=*create_signal(color))
+                            {
+                                TableBody
+                                {
+                                    Indexed
+                                    (
+                                        iterable=*iter,
+                                        view=move |i| view!
+                                        {
+                                            TableRow
+                                            {
+                                                TableCell(is_head=*create_signal(true))
+                                                {
+                                                    "Header " (i)
+                                                }
+                                                Indexed
+                                                (
+                                                    iterable=*iter,
+                                                    view=move |j| view!
+                                                    {
+                                                        TableCell
+                                                        {
+                                                            "Data " (i) "-" (j)
+                                                        }
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            )
         }
     }
 }
